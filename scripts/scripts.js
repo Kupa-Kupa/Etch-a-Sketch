3d3a772ced94954ee8c8068ca71aa020d3b8bdd0
cooper <cooper.elliott30@gmail.com>
2022-11-29 22:15:08 +1100

    Added darken functionality
    Added rgb to hsl conversion function so that i can edit the l value
    and darken the background colour of squares.
    
    Had an issue getting the background colour of the squares as
    event.target.style.backgroundColor returned an empty string for some
    reason. Had to use
    getComputedStyle(event.target).getPropertyValue('background-color')

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,89 @@
+/*--- Global Variables ---*/
+let colour = `rgb(0,0,0)`;
+
+let rainbow = false;
+
+let darken = false;
+
+let column;
+
+
 /* sidebar */
 let sidebar = document.querySelector(".side-bar");
+
+let blackButton = document.querySelector("button#black");
+
+let eraserButton = document.querySelector("button#eraser");
+
+let colourButton = document.querySelector("button#colour");
+
+let rainbowButton = document.querySelector("button#rainbow");
+
+let darkenButton = document.querySelector("button#darken");
+
+let lightenButton = document.querySelector("button#lighten");
+
+
+/*--- Button Colours ---*/
+
+blackButton.addEventListener("click", () => {
+    colour = `rgb(0,0,0)`;
+    rainbow = false;
+});
+
+eraserButton.addEventListener("click", () => {
+    colour = `rgb(255,255,255)`;
+    rainbow = false;
+});
+
+colourButton.addEventListener("click", () => {
+    colour = `rgb(255,255,255)`;
+    rainbow = false;
+});
+
+rainbowButton.addEventListener("click", () => {
+    colour = 'rainbow';
+    rainbow = true;
+    // console.log(randomRGB());
+});
+
+
+/*  to lighten and darken I can:
+    1) convert rgb to hsl which seems complex
+    2) stack a grid on top of grid, change the colour of the base grid 
+    and then change opacity of the top grid so the base grid colour comes 
+    through
+    3) convert to hex?
+*/
+darkenButton.addEventListener("click", () => {
+    colour = `rgb(255,255,255)`;
+    rainbow = false;
+    darken = true;
+});
+
+lightenButton.addEventListener("click", () => {
+    colour = `rgb(0,0,0)`;
+    rainbow = false;
+});
+
+
+function randomRGB(){
+    return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+}
+
+
+function getColour(){
+    if (colour === 'rainbow') {
+        colour = randomRGB();
+        return colour;
+    } else {
+        return colour;
+    }
+}
+
+
+
+
 
 
 
@@ -22,10 +106,13 @@ for(let i = 0; i < 16; i++) {
     }
 }
 
-/*--- disable grab functionality ---*/
-gridContainer.addEventListener("drag", (event) => {
-    event.preventDefault();
-});
+// can comment out
+column = document.querySelector(".column");
+
+/*--- disable right click on grid ---*/
+// gridContainer.addEventListener("contextmenu", (event) => {
+//     event.preventDefault();
+// });
 
 
 /* Slider */
@@ -71,7 +158,8 @@ function resizeGrid(){
         }
     }
 
-    // let column = document.querySelector(".column");
+    // can comment out
+    column = document.querySelector(".column");
 
     // column.addEventListener("mouseenter", colorChange);
 
@@ -182,23 +270,141 @@ gridContainer.addEventListener("mousedown", colorChange);
 
 function colorChange(event) {
 
+    // rainbow
+    if(event.target.classList.contains('column') && mouseDown == true && event.type === `mouseover` && rainbow === true) {
+        colour = randomRGB();
+        // event.target.style.backgroundColor = colour;
+        event.target.setAttribute('style', `background-color: ${colour};`);
+    } else if (event.target.classList.contains('column') && event.type === `mousedown` && rainbow === true) {
+        event.preventDefault();
+        colour = randomRGB();
+        // event.target.style.backgroundColor = colour; 
+        event.target.setAttribute('style', `background-color: ${colour};`);
+    }
+
+
+
+    // darken
+    /* having big issues trying to get the background colour of the square 
+    */
+    if(event.target.classList.contains('column') && mouseDown == true && event.type === `mouseover` && darken === true) {
+        console.log(event.target.style.backgroundColor);
+        colour = darkenColour(rgbToHsl(event.target.style.backgroundColor));
+        console.log(colour);
+        event.target.style.backgroundColor = colour; 
+    } else if (event.target.classList.contains('column') && event.type === `mousedown` && darken === true) {
+        event.preventDefault();
+        console.log(event.target);
+
+        // gives correct answer of white rgb
+        console.log(getComputedStyle(event.target).getPropertyValue('background-color'));
+
+        // logs empty string
+        console.log(event.target.style.backgroundColor);
+
+        // doesn't work
+        // colour = darkenColour(rgbToHsl(event.target.style.backgroundColor));
+        
+        
+        colour = darkenColour(rgbToHsl(getComputedStyle(event.target).getPropertyValue('background-color')));
+
+
+
+        console.log(colour);
+        event.target.style.backgroundColor = colour;
+        console.log(event.target.style.backgroundColor);
+    }
+
+
+
+
+
+    
     // change tile background on click and move
     if(event.target.classList.contains('column') && mouseDown == true) {
-        event.target.style.backgroundColor = "red";      
+        event.target.style.backgroundColor = colour;      
     }
     
     // change single tile background on click
     if(event.target.classList.contains('column') && event.type === `mousedown`) {
         // prevent draggable functionality ("grabbing")
         event.preventDefault();
-        event.target.style.backgroundColor = "red"; 
+        event.target.style.backgroundColor = colour; 
     }
-    // event.target.style.backgroundColor = "red";
     // console.log(event.target)
     // console.log('enter');
+
+    
+
+    
 }
 
 
 
 
+function rgbToHsl(colour){
+    // regex: match any single character that's not 0-9 , or .
+    console.log(typeof(colour));
+    let rgb = colour.replace(/[^\d,.]/g, '').split(',');
+    console.log(rgb);
+    let r = parseInt(rgb[0]);
+    let g = parseInt(rgb[1]);
+    let b = parseInt(rgb[2]);
+    
+    let newR = r / 255;
+    let newG = g / 255;
+    let newB = b / 255;
 
+    let min = Math.min(newR, newB, newG);
+    console.log(min);
+    let max = Math.max(newR, newB, newG);
+    console.log(max);
+
+    let delta = max - min
+
+    let l = (max + min) / 2;
+    console.log(`l:${l}`);
+
+    let h;
+    let s;
+
+    if (delta === 0) {
+        s = 0;
+        h = 0;
+    } else {
+        s = (delta / (1 - Math.abs(2 * l - 1)))
+        console.log(`s:${s}`);
+
+        if(max === newR) {
+            h = 60 * (((newG - newB) / delta));
+
+            if(h < 0) {
+                h = h + 360;
+            }
+
+            console.log(`h:${h}`);
+        } else if (max === newG) {
+            h = 60 * (((newB - newR) / delta) + 2);
+            console.log(`h:${h}`);
+        } else if (max === newB) {
+            h = 60 * (((newR - newG) / delta) + 4);
+            console.log(`h:${h}`);
+        }
+    }
+
+    console.log(`hsl(${h},${s},${l})`);
+
+    return `hsl(${h},${s},${l})`;
+}
+
+// rgbToHsl(`rgb(0,200,0)`);
+
+
+function darkenColour(hsl) {
+    let hslArray = hsl.replace(/[^\d,.]/g, '').split(',');
+    console.log(hslArray);
+    console.log(hslArray[2]);
+    let darkenL = hslArray[2] - 0.1;
+    let darkHsl = `hsl(${hslArray[0]}, ${hslArray[1] * 100}%, ${darkenL * 100}%)`;
+    return darkHsl;
+}
